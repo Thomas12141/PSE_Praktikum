8b00bbb83018d8e6114413de018d39375b474e2a
Jeremy Beltran <jeremy@Jeremys-MacBook-Pro.local>
2023-04-12 19:24:07 +0200

    Struct für HTTP-RESPONSE Hinzugefügt

--- httplib.h
+++ httplib.h
@@ -14,6 +14,26 @@ struct string {
 };
 typedef struct string string;
 
+
+struct http_response_header {
+
+    float http_version;
+    int status_code;
+    string reason_phrase;
+    size_t content_length;
+    string content_type;
+};
+typedef struct http_response_header http_response_header;
+
+struct http_response {
+
+    http_response_header header;
+    string http_body;
+};
+typedef struct http_response http_response;
+
+
+
 string* str_cat(string* dest, const char* src, size_t len);
 string* _new_string();
 void print_string(string* str);
