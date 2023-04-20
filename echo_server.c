e94ed994cc34b1070071bd3222bc119a1a6aabf9
matteoilling <matteo.illing@icloud.com>
2023-04-20 22:18:16 +0200

    Refactoring: renamed most of httplib to strlib, extract config macros, add validation files for corresponding functions

--- echo_server.c
+++ echo_server.c
@@ -7,34 +7,9 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <sys/syslimits.h>
 #include "httplib.h"
-
-#define PORT 31337
-#define BUFFER_SIZE 1024*1024
-#define DOCROOT "../htdocs"
-
-/**
- * Überprüft, ob eine Datei existiert.
- * @param filename Dateiname im Docroot.
- * @return Wahrheitswert (1 = datei existiert, 0 = datei existiert nicht).
-*/
-int isFileExistent(string* filename) {
-    //Zusammenfügen von DOCROOT (Verzeichnis) und string (html-Datei) für fopen.
-    string* docroot = cpy_str(DOCROOT, strlen(DOCROOT));
-    string* file_path = str_cat(docroot, filename->str, filename->len);
-
-    char* filepathPointer = calloc(file_path->len + 1, 1);
-    memcpy(filepathPointer, file_path->str, file_path->len);
-
-    //Prüfen, ob die Datei existiert.
-    FILE *file = fopen(filepathPointer, "r");;
-    if (file != NULL) {
-        fclose(file);
-        return 1; // Datei existiert.
-    }
-    return 0; // Datei existiert nicht.
-}
+#include "validation.h"
+#include "config.h"
 
 string* process(string *request);
 
@@ -244,30 +219,6 @@ static void main_loop() {
     }
 }
 
-/**
- * Die Funktion akzeptiert den eingehenden Request und gibt eine entsprechende Response zurück.
- * @param request Der eingehende Request.
- * @return int (1 = ist valid, 0 = ist nicht valid, z.B. außerhalb des docroots).
- */
-int isResourceValid(string* resource_path) {
-    char pathBuffer [PATH_MAX+1];
-    char* ptr = realpath(DOCROOT, pathBuffer);
-
-    string* docroot = cpy_str(DOCROOT, strlen(DOCROOT));
-    string* file_path = str_cat(docroot, resource_path->str, resource_path->len);
-    string* docrootPathString = cpy_str(ptr, strlen(ptr));
-
-    char* filepathPointer = calloc(file_path->len + 1, 1);
-    memcpy(filepathPointer, file_path->str, file_path->len);
-
-    char* requestedFilePath = realpath(filepathPointer, pathBuffer);
-    string* filePathString = cpy_str(requestedFilePath, strlen(requestedFilePath));
-    free(filepathPointer);
-
-    int res = memcmp(filePathString->str, docrootPathString->str, docrootPathString->len);
-
-    return res == 0;
-}
 string* process(string *request) {
     /*
      * Diese Funktion müssen Sie anpassen, so dass der request von Ihrem Code verarbeitet wird,
---
+++ strlib.h
@@ -0,0 +1,29 @@
+//
+// Created by Matteo Illing on 20.04.23.
+//
+
+#ifndef PSE_WEBSERVER_STRLIB_H
+#define PSE_WEBSERVER_STRLIB_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct string {
+    size_t len;
+    char* str;
+};
+typedef struct string string;
+
+
+string* str_cat(string* dest, const char* src, size_t len);
+string* _new_string();
+void print_string(string* str);
+string* cpy_str(const char* src, size_t len);
+void free_str(string* str);
+size_t get_length(string* str);
+char* get_char_str(string* str);
+int str_cmp(string* str1, string* str2);
+
+#endif //PSE_WEBSERVER_STRLIB_H
