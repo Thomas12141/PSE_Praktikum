77b798f126b1d7adeded54655d948b4d9e4d7a42
matteoilling <matteo.illing@icloud.com>
2023-04-22 13:34:43 +0200

    Added getResponseStr with output of content-type, content-length and body

--- httplib.h
+++ httplib.h
@@ -24,7 +24,7 @@ struct http_response_header {
     string* protocol; //HTTP Version z. B. HTTP/1.1
     string* status_code; // 3-stellige Ganzzahl zur kurzen Beschreibung des Zustandes
     string* reason_phrase; // Beschreibung des Zustandes in Textform
-    size_t content_length;  // Leange des Inhalts
+    unsigned long content_length;  // Leange des Inhalts
     string* content_type;   // Art des Inhalts
 };
 typedef struct http_response_header http_response_header;
@@ -37,4 +37,5 @@ typedef struct http_response http_response;
 
 http_request* getRequestStruct(string* str);
 char* getFilePath(http_request* request);
+string* getResponseString(http_response* response);
 #endif //ECHO_SERVER_HTTPLIB_H
