c8a77dbc35629d022ea7d7e768f5ad030f56cb49
simon <simon@LAPTOP-62KP58RO.localdomain>
2023-04-24 19:41:21 +0200

    Doku von isFileExistent und isFileAccessible

--- validation.c
+++ validation.c
@@ -2,6 +2,7 @@
 // Created by Matteo Illing on 20.04.23.
 //
 
+#include <linux/limits.h>
 #include "validation.h"
 
 int isFileInsideDocroot(char* filepath) {
@@ -13,6 +14,12 @@ int isFileInsideDocroot(char* filepath) {
     return res == 0;
 }
 
+/**
+ * Überprüft, ob die Datei gelesen werden kann, indem versucht wird mit access (Typ R_OK) auf die Datei zuzugreifen.
+ *
+ * @param filepath Der Dateipfad der zu überprüfenden Datei.
+ * @return 1, wenn auf die Datei zugegriffen werden kann, 0 wenn nicht.
+ */
 int isFileAccessible(char* filepath) {
     if(!access(filepath, R_OK)) {
         return 1;
@@ -21,6 +28,12 @@ int isFileAccessible(char* filepath) {
     return 0;
 }
 
+/**
+ *Überprüft, ob eine Datei existiert, indem versucht wird mit access (Typ F_OK) auf die Datei zuzugreifen.
+ *
+ * @param filepath Der Dateipfad der zu überprüfenden Datei.
+ * @return 1, wenn die Datei existiert, 0 wenn nicht.
+ */
 int isFileExistent(char* filepath) {
     if(!access(filepath, F_OK)) {
         return 1;
